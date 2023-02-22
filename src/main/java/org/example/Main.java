246e949fd1689e60e9d73f294bd60294787e982b
alexandr1283 <aw.shabanau@gmail.com>
2023-02-22 10:13:34 +0300

    add cycle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello CodGit!");
+        int a = 3;
+        for(int i = 5; i<=10; i++){
+            System.out.println("codding " + i);
+            i = i + a;
+        }
     }
 }
